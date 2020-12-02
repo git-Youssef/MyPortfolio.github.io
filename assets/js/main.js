eb3ef237fdbce311f1f2f2a5a42bd75a96c5c587
Youssef Karfouh <63546757+git-Youssef@users.noreply.github.com>
2020-12-02 09:55:49 +0100

    added files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+(function ($) {
+
+  "use strict";
+
+  //change active class 
+
+  $(".smoothScroll").click(function () {
+
+    let href = $(this).attr("href");
+
+
+    if ($(this).closest(".nav-links").length > 0) {
+      $(".nav-links a").removeClass("active");
+      $(this).addClass("active")
+    }
+
+    $('html, body').animate({
+      scrollTop: (href === "#") ? 0 : $(href).offset().top,
+    }, 1000);
+
+  })
+
+  // toggle opened  class 
+  $(".menu-toggler").click(function () {
+
+    $(".nav-bar").toggleClass("opened");
+    $(".nav-links").slideToggle(500);
+
+  })
+
+  // typed text
+  var typed = new Typed('.typed', {
+    strings: ["Front End Developer.", "Web Integrator."],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 3000,
+    loop: true
+  });
+
+  //target tooltip
+  $('[data-toggle]').tooltip();
+
+ 
+  //intilaize magnifique popup 
+
+  $('#portfolio-container').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery:{
+      enabled:true
+    },
+    
+  });
+
+})(jQuery);
